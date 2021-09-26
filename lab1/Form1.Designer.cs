3213c2257ca70203db395a32eeb17ac30a1a9f16
AndreyPC <tigra9900@mail.ru>
2021-09-26 17:11:57 +1000

    Merge Project

--- lab1/Form1.Designer.cs
+++ lab1/Form1.Designer.cs
@@ -29,40 +29,55 @@ namespace lab1
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title4 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea1.AxisX.LabelStyle.Format = "0.00";
-            chartArea1.AxisY.LabelStyle.Format = "0.00";
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
+            chartArea4.AxisX.LabelStyle.Format = "0.00";
+            chartArea4.AxisY.LabelStyle.Format = "0.00";
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
             this.chart1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.chart1.Location = new System.Drawing.Point(0, 61);
+            this.chart1.Location = new System.Drawing.Point(3, 59);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(484, 500);
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(500, 500);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
-            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            title1.Name = "Title1";
-            title1.Text = "Пока ничего, но что-то да будет";
-            this.chart1.Titles.Add(title1);
+            title4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            title4.Name = "Title1";
+            title4.Text = "Пока ничего, но что-то да будет";
+            this.chart1.Titles.Add(title4);
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(8, 8);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 40);
             this.button1.TabIndex = 1;
@@ -73,7 +88,7 @@ namespace lab1
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.button2.Location = new System.Drawing.Point(118, 12);
+            this.button2.Location = new System.Drawing.Point(114, 8);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 40);
             this.button2.TabIndex = 2;
@@ -81,17 +96,135 @@ namespace lab1
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(514, 591);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.chart1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(506, 562);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "График функции";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(506, 562);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Многоугольник";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage3.Controls.Add(this.button6);
+            this.tabPage3.Controls.Add(this.button7);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(506, 562);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Алгоритм";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button3.Location = new System.Drawing.Point(8, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 40);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Вариант 9";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button4.Location = new System.Drawing.Point(114, 8);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 40);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Вариант 19";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button5.Location = new System.Drawing.Point(220, 8);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 40);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Вариант 23";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button6.Location = new System.Drawing.Point(8, 8);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 40);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Вариант 9";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button7.Location = new System.Drawing.Point(114, 8);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 40);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Вариант 12";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 561);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.chart1);
+            this.ClientSize = new System.Drawing.Size(514, 591);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
-            this.Text = "График функции";
+            this.Text = "Графические функции";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -101,6 +234,16 @@ namespace lab1
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- lab1/Form1.cs
+++ lab1/Form1.cs
@@ -12,6 +12,10 @@ namespace lab1
 {
     public partial class Form1 : Form
     {
+        // второе задание
+        private const int sizeStar = 50;  // увеличение размера звезды (по десятке добавлять)
+        private const int correctSize = sizeStar / 10 * sizeStar;  // авто коррекция местоположения звезды
+
         public Form1()
         {
             InitializeComponent();
@@ -48,6 +52,219 @@ namespace lab1
 
                 this.chart1.Series[0].Points.AddXY(x, y);
             }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Без bitmap появляются мерцания при рисовке изображения
+            Bitmap myBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height); // буфер для Bitmap-изображения
+            Graphics graph = Graphics.FromImage(myBitmap);                       // графический объект — некий холст
+            graph.Clear(Color.White);
+
+            Brush blackBrush = new SolidBrush(Color.Black);
+            Brush whiteBrush = new SolidBrush(Color.White);
+            Pen blackPen = new Pen(Color.Black, 2);
+
+            drawLetterI(graph, blackBrush);    // рисуем буквы
+            drawStar(graph, blackPen);         // рисуем звезду
+            fillBackground(graph, whiteBrush); // заливаем фон звезды белым цветом
+
+            pictureBox1.Image = myBitmap;
+        }        
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Без bitmap появляются мерцания при рисовке изображения
+            Bitmap myBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height); // буфер для Bitmap-изображения
+            Graphics graph = Graphics.FromImage(myBitmap);                       // графический объект — некий холст
+            graph.Clear(Color.White);
+
+            Brush blackBrush = new SolidBrush(Color.Black);
+            Brush whiteBrush = new SolidBrush(Color.White);
+            Pen blackPen = new Pen(Color.Black, 2);
+
+            drawLetterS(graph, blackPen);      // рисуем буквы
+            drawStar(graph, blackPen);         // рисуем звезду
+            fillBackground(graph, whiteBrush); // заливаем фон звезды белым цветом
+
+            pictureBox1.Image = myBitmap;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Без bitmap появляются мерцания при рисовке изображения
+            Bitmap myBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height); // буфер для Bitmap-изображения
+            Graphics graph = Graphics.FromImage(myBitmap);                       // графический объект — некий холст
+            graph.Clear(Color.White);
+
+            Brush blackBrush = new SolidBrush(Color.Black);
+            Brush whiteBrush = new SolidBrush(Color.White);
+            Pen blackPen = new Pen(Color.Black, 2);
+
+            drawLetterW(graph, blackPen);      // рисуем буквы
+            drawStar(graph, blackPen);         // рисуем звезду
+            fillBackground(graph, whiteBrush); // заливаем фон звезды белым цветом
+
+            pictureBox1.Image = myBitmap;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void drawLetterW(Graphics graph, Pen blackPen)
+        {
+            const int sizeLetter = 8;             // размер буквы
+            const int distanceX = sizeLetter * 3; // дистанция между буквами по координате X
+            const int distanceY = sizeLetter * 3; // дистанция между буквами по координате Y
+            const int widthLetter = 2;            // высота буквы
+            const int heightLetter = 3;           // ширина буквы
+
+            const int startPosX = 0;              // стартовая позиция рисования букв по X
+            const int startPosY = 0;              // стартовая позиция рисования букв по Y
+            const int firstCoordX = sizeLetter;
+            const int bottomCoordY = firstCoordX;
+            const int topCoordY = firstCoordX * widthLetter;
+
+            const int secondCoordX = firstCoordX + heightLetter;
+            const int thirdCoordX = secondCoordX + heightLetter;
+            const int fourthCoordX = thirdCoordX + heightLetter;
+            const int fifthCoordX = fourthCoordX + heightLetter;
+
+            for (int y = startPosY; y <= pictureBox1.Height; y += distanceY)
+            {
+                for (int x = startPosX; x <= pictureBox1.Width; x += distanceX)
+                {
+                    graph.DrawLines(blackPen, new PointF[]
+                    {
+                        new Point(firstCoordX + x, bottomCoordY + y),
+                        new Point(secondCoordX + x, topCoordY + y),
+                        new Point(thirdCoordX + x, bottomCoordY + y),
+                        new Point(fourthCoordX + x, topCoordY + y),
+                        new Point(fifthCoordX + x, bottomCoordY + y),
+                    });
+                }
+            }
+        }
+
+        private void drawLetterS(Graphics graph, Pen blackPen)
+        {
+            const int sizeLetter = 8;             // размер буквы
+            const int distanceX = sizeLetter * 3; // дистанция между буквами по координате X
+            const int distanceY = sizeLetter * 3; // дистанция между буквами по координате Y
+
+            const int startPosX = 8;              // стартовая позиция рисования букв по X
+            const int startPosY = 0;              // стартовая позиция рисования букв по Y
+            const int weightArc = sizeLetter;                        // высота полукруга
+            const int heightArc = (sizeLetter + sizeLetter * 2) / 2; // ширина полукруга
+
+            const int coordYfirstArc = sizeLetter;
+            const int coordYsecondArc = sizeLetter * 2 - sizeLetter / 4;
+            const int beginSecondArc = heightArc / 2;
+
+            for (int y = startPosY; y <= pictureBox1.Height; y += distanceY)
+            {
+                for (int x = startPosX; x <= pictureBox1.Width; x += distanceX)
+                {
+                    graph.DrawArc(blackPen, x, coordYfirstArc + y, heightArc, weightArc, 90, 180);
+                    graph.DrawArc(blackPen, x - beginSecondArc, coordYsecondArc + y, heightArc, weightArc, -90, 180);
+                }
+            }
+        }
+
+        private void drawLetterI(Graphics graph, Brush blackBrush)
+        {
+            const int startPos = 8;             // стартовая позиция рисования букв (изменение масштаба буквы)
+            const int distance = startPos * 3;  // дистанция между буквами
+            const int width = 2;                // ширина буквы
+
+            for (int w = startPos; w <= pictureBox1.Size.Width + startPos; w = w + distance)
+            {
+                for (int h = startPos; h <= pictureBox1.Size.Width + startPos; h = h + distance)
+                {
+                    for (int i = h; i <= h + startPos; i = i + 2)
+                    {
+                        graph.FillRectangle(blackBrush, i, w, width, width);
+                        graph.FillRectangle(blackBrush, i, w + startPos, width, width);
+                        graph.FillRectangle(blackBrush, w + startPos / 2, i, width, width);
+                    }
+                }
+            }
+        }
+
+        private void drawStar(Graphics graph, Pen blackPen)
+        {
+            graph.DrawLines(blackPen, new PointF[]
+                {
+                new Point(10 * sizeStar - correctSize, 8 * sizeStar - correctSize),
+                new Point(13 * sizeStar - correctSize, 6 * sizeStar - correctSize),
+                new Point(12 * sizeStar - correctSize, 9 * sizeStar - correctSize),
+                new Point(14 * sizeStar - correctSize, 11 * sizeStar - correctSize),
+                new Point(11 * sizeStar - correctSize, 11 * sizeStar - correctSize),
+                new Point(10 * sizeStar - correctSize, 14 * sizeStar - correctSize),
+                new Point(9 * sizeStar - correctSize, 11 * sizeStar - correctSize),
+                new Point(6 * sizeStar - correctSize, 11 * sizeStar - correctSize),
+                new Point(6 * sizeStar - correctSize, 11 * sizeStar - correctSize),
+                new Point(8 * sizeStar - correctSize, 9 * sizeStar - correctSize),
+                new Point(7 * sizeStar - correctSize, 6 * sizeStar - correctSize),
+                new Point(10 * sizeStar - correctSize, 8 * sizeStar - correctSize),
+             });
+        }
+
+        private void fillBackground(Graphics graph, Brush whiteBrush)
+        {
+            // Нижний многоугольник (за фигурой)
+            graph.FillPolygon(whiteBrush, new PointF[]
+            {
+                new Point(11 * sizeStar - correctSize, 11 * sizeStar - correctSize),
+                new Point(10 * sizeStar - correctSize, 14 * sizeStar - correctSize),
+                new Point(9 * sizeStar - correctSize, 11 * sizeStar - correctSize),
+                new Point(0, 11 * sizeStar - correctSize),
+                new Point(0, pictureBox1.Size.Height),
+                new Point(pictureBox1.Size.Width, pictureBox1.Size.Height),
+                new Point(pictureBox1.Size.Width, 11 * sizeStar - correctSize),
+                new Point(11 * sizeStar - correctSize, 11 * sizeStar - correctSize),
+            });
+
+            // Левый многоугольник (за фигурой)
+            graph.FillPolygon(whiteBrush, new PointF[]
+            {
+                new Point(6 * sizeStar - correctSize, 11 * sizeStar - correctSize),
+                new Point(6 * sizeStar - correctSize, 11 * sizeStar - correctSize),
+                new Point(8 * sizeStar - correctSize, 9 * sizeStar - correctSize),
+                new Point(7 * sizeStar - correctSize, 6 * sizeStar - correctSize),
+                new Point(0, 0),
+                new Point(0, 11 * sizeStar - correctSize),
+                new Point(6 * sizeStar - correctSize, 11 * sizeStar - correctSize),
+            });
+
+            // Верхний многоугольник (за фигурой)
+            graph.FillPolygon(whiteBrush, new PointF[]
+            {
+                new Point(7 * sizeStar - correctSize, 6 * sizeStar - correctSize),
+                new Point(10 * sizeStar - correctSize, 8 * sizeStar - correctSize),
+                new Point(13 * sizeStar - correctSize, 6 * sizeStar - correctSize),
+                new Point(pictureBox1.Size.Width, 0),
+                new Point(0, 0),
+                new Point(7 * sizeStar - correctSize, 6 * sizeStar - correctSize),
+            });
+
+            // Правый многоугольник (за фигурой)
+            graph.FillPolygon(whiteBrush, new PointF[]
+            {
+                new Point(13 * sizeStar - correctSize, 6 * sizeStar - correctSize),
+                new Point(12 * sizeStar - correctSize, 9 * sizeStar - correctSize),
+                new Point(14 * sizeStar - correctSize, 11 * sizeStar - correctSize),
+                new Point(pictureBox1.Size.Width, 11 * sizeStar - correctSize),
+                new Point(pictureBox1.Size.Width, 0),
+                new Point(13 * sizeStar - correctSize, 6 * sizeStar - correctSize),
+            });
         }
     }
 }
