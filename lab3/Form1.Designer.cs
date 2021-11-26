0fc69458c5411bd462cb92b35de34749872ab6cb
AndreyPC <tigra9900@mail.ru>
2021-11-26 22:31:32 +1000

    line4 line8 fillLineWithSeed

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -29,36 +29,230 @@ namespace lab3
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pbFirst = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pbSecond = new System.Windows.Forms.PictureBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.pbThird = new System.Windows.Forms.PictureBox();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pbCircle = new System.Windows.Forms.PictureBox();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.pbFirstExample = new System.Windows.Forms.PictureBox();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.pbSecondExample = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFirst)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSecond)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbThird)).BeginInit();
+            this.tabPage6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCircle)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFirstExample)).BeginInit();
+            this.tabPage5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSecondExample)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // tabControl1
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(654, 531);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage1.Controls.Add(this.pbFirst);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.tabPage1.Size = new System.Drawing.Size(646, 502);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Первый";
+            // 
+            // pbFirst
+            // 
+            this.pbFirst.Location = new System.Drawing.Point(0, 0);
+            this.pbFirst.Margin = new System.Windows.Forms.Padding(5);
+            this.pbFirst.Name = "pbFirst";
+            this.pbFirst.Size = new System.Drawing.Size(646, 502);
+            this.pbFirst.TabIndex = 5;
+            this.pbFirst.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage2.Controls.Add(this.pbSecond);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.tabPage2.Size = new System.Drawing.Size(646, 502);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Второй";
+            // 
+            // pbSecond
+            // 
+            this.pbSecond.Location = new System.Drawing.Point(0, 0);
+            this.pbSecond.Margin = new System.Windows.Forms.Padding(5);
+            this.pbSecond.Name = "pbSecond";
+            this.pbSecond.Size = new System.Drawing.Size(646, 502);
+            this.pbSecond.TabIndex = 5;
+            this.pbSecond.TabStop = false;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage3.Controls.Add(this.pbThird);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(646, 502);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Третий";
+            // 
+            // pbThird
+            // 
+            this.pbThird.Location = new System.Drawing.Point(0, 0);
+            this.pbThird.Margin = new System.Windows.Forms.Padding(5);
+            this.pbThird.Name = "pbThird";
+            this.pbThird.Size = new System.Drawing.Size(646, 502);
+            this.pbThird.TabIndex = 6;
+            this.pbThird.TabStop = false;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage6.Controls.Add(this.btnStart);
+            this.tabPage6.Controls.Add(this.pbCircle);
+            this.tabPage6.Location = new System.Drawing.Point(4, 25);
+            this.tabPage6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Size = new System.Drawing.Size(646, 502);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "Дополнительное";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(717, 15);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(133, 31);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "Запуск";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // pbCircle
+            // 
+            this.pbCircle.Location = new System.Drawing.Point(0, 0);
+            this.pbCircle.Margin = new System.Windows.Forms.Padding(5);
+            this.pbCircle.Name = "pbCircle";
+            this.pbCircle.Size = new System.Drawing.Size(646, 502);
+            this.pbCircle.TabIndex = 7;
+            this.pbCircle.TabStop = false;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage4.Controls.Add(this.pbFirstExample);
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(646, 502);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Пример1";
+            // 
+            // pbFirstExample
+            // 
+            this.pbFirstExample.Location = new System.Drawing.Point(0, 0);
+            this.pbFirstExample.Margin = new System.Windows.Forms.Padding(5);
+            this.pbFirstExample.Name = "pbFirstExample";
+            this.pbFirstExample.Size = new System.Drawing.Size(646, 502);
+            this.pbFirstExample.TabIndex = 6;
+            this.pbFirstExample.TabStop = false;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage5.Controls.Add(this.pbSecondExample);
+            this.tabPage5.Location = new System.Drawing.Point(4, 25);
+            this.tabPage5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(646, 502);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Пример2";
+            // 
+            // pbSecondExample
+            // 
+            this.pbSecondExample.Location = new System.Drawing.Point(0, 0);
+            this.pbSecondExample.Margin = new System.Windows.Forms.Padding(5);
+            this.pbSecondExample.Name = "pbSecondExample";
+            this.pbSecondExample.Size = new System.Drawing.Size(646, 502);
+            this.pbSecondExample.TabIndex = 6;
+            this.pbSecondExample.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(654, 531);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximumSize = new System.Drawing.Size(670, 570);
+            this.MinimumSize = new System.Drawing.Size(670, 570);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Построение многоугольников";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbFirst)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSecond)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbThird)).EndInit();
+            this.tabPage6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCircle)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbFirstExample)).EndInit();
+            this.tabPage5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSecondExample)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.PictureBox pbFirst;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pbSecond;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.PictureBox pbThird;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pbCircle;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.PictureBox pbFirstExample;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.PictureBox pbSecondExample;
     }
 }
 
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -1,52 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace lab3
 {
     public partial class Form1 : Form
     {
-		// Common names
-		int algor = 1;  // по умолчанию симметричный ЦДА
-		int maxX, maxY;     // размеры окна
-		int x0, y0; //Координаты центра системы координат относит. лев. верх. угла
-		int hx = 5, hy = 5; //1/2 ширины и длины пиксела
-							//int x[100], y[100]; 
-		int count;
+		Graphics graph;
+		Bitmap myBitmap;
+
+		int maxX, maxY;     // Размеры PictureBox
+		int x0, y0;         // Координаты центра PictureBox
+		int hx = 3, hy = 3; // Половина от длины и ширины пикселя
 
 		public Form1()
         {
             InitializeComponent();
 
-			maxX = pictureBox1.Width;
-			maxY = pictureBox1.Height;
+			// Без bitmap появляются мерцания при рисовке изображения
+			myBitmap = new Bitmap(pbFirst.Width, pbFirst.Height); // буфер для Bitmap-изображения
+			graph = Graphics.FromImage(myBitmap);                       // графический объект — некий холст
+			graph.Clear(Color.White);
 
-			//Form1->Image1->Canvas->Brush->Color = clBlack;
-			x0 = maxX / 2; 
-			y0 = maxY / 2;
-
-			count = 1;  // симметричный ЦДА по умолчанию
-			//Form1->Label1->Caption = "Symmetric DDA";
+			maxX = pbFirst.Width;  // ширина 
+			maxY = pbFirst.Height; // высота
+			x0 = maxX / 2; y0 = maxY / 2;
 
 			line(x0, 0, x0, maxY); 
 			line(0, y0, maxX, y0);
 
+			
+			line8(0, -30, 30, 20);
+			line8(30, 20, -30, 30);
+			line8(-30, 30, 0, -30);
+
+			Pen myPen = new Pen(Color.Red);
+
+			
+			
+
+			/*
+			line4(0, -10, 20, 20);
+			line4(20, 20, -10, 10);
+			line4(-10, 10, 0, -10);
+			*/
+
+			Color colorRed = Color.FromArgb(255, 255, 0, 0); // красный цвет
+			Color colorBlack = Color.FromArgb(255, 0, 0, 0); // черный цвет
+
+			pbFirst.Image = myBitmap;
+
+			fillLineWithSeed(colorBlack, colorRed, x0, y0);
+
+
+			
+			
+
 		}
 
-		// вывод прямых
-		void line(int x0, int y0, int x1, int y1)
-		{
-			//Form1->Image1->Canvas->MoveTo(x0, y0); // начало отрезка
-			//Form1->Image1->Canvas->LineTo(x1, y1); // вывод отрезка
-		}
-
-		//Функция определения знака целого числа
+		// Функция определения знака целого числа
 		int sign(int r)
 		{
 			if (r > 0) return 1;
@@ -54,156 +67,134 @@ namespace lab3
 			else return 0;
 		}
 
-		// Вывод "крупного" пиксела на экран
+		// вывод прямых
+		void line(int x0, int y0, int x1, int y1)
+		{
+			Pen grayPen = new Pen(Color.Gray);
+			graph.DrawLine(grayPen, x0, y0, x1, y1);
+			// Form1->Image1->Canvas->MoveTo(x0, y0); // начало отрезка
+			// Form1->Image1->Canvas->LineTo(x1, y1); // вывод отрезка
+		}
+
+		//Вывод "крупного" пиксела на экран
 		void point(int ix, int iy)
 		{
-			// Без bitmap появляются мерцания при рисовке изображения
-			Bitmap myBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height); // буфер для Bitmap-изображения
-			Graphics graph = Graphics.FromImage(myBitmap);                       // графический объект — некий холст
-			graph.Clear(Color.White);
+			// TRect t;
+			// t = Rect(x0 + hx * 2 * ix - hx, y0 - hy * 2 * iy + hy, x0 + hx * 2 * ix + hx - 1, y0 - hy * 2 * iy - hy + 1);
 
 			Brush blackBrush = new SolidBrush(Color.Black);
+			Pen blackPen = new Pen(Color.Black);
 
-			//graph.FillRectangle(blackBrush, 2,2,2,2);
-			// graph.FillRectangle(blackBrush, x0 + hx * 2 * ix - hx, y0 - hy * 2 * iy + hy, x0 + hx * 2 * ix + hx - 1, y0 - hy * 2 * iy - hy + 1);
+			Point[] point = new Point[]
+			{
+				new Point(x0 + hx * 2 * ix - hx, y0 - hy * 2 * iy + hy),
+                new Point(x0 + hx * 2 * ix + hx - 1, y0 - hy * 2 * iy - hy + 1)
+            };
 
-			graph.FillRectangle(blackBrush, x0 + hx * 2 * ix - hx, y0 - hy * 2 * iy + hy, x0 + hx * 2 * ix + hx - 1, y0 - hy * 2 * iy - hy + 1);
+			graph.FillPolygon(blackBrush, point);
 
-			pictureBox1.Image = myBitmap;
+
+			Rectangle pointR = new Rectangle(x0 + hx * 2 * ix - hx, y0 - hy * 2 * iy + hy, hx * 2, hy * 2);
+			
+			graph.FillRectangle(blackBrush, pointR);
+		    // graph.DrawRectangle(blackPen, pointR);
+
+
+			// Form1->Image1->Canvas->FillRect(t);
 		}
 
-		//Генерация точек кривой методом приращений
-		void lineg()//Метод приращений
-		{
-			int x, y;
-			float a = 44f;
-			float delta;
-			hx = 3; hy = 3;
-			x0 = 20; y0 = pictureBox1.Height - 20;
-			x = 0; y = 0; delta = 0;        //начальная точка (x0,y0))
-			while (2 * a * x < 1)           //пока вектор касательной принадлежит 1-й
-			{               //октанте
-				point(x, y);        //ставим точку с координатами (x,y)
-				if (delta < 0)
-					delta += a * (2 * x + 1); //положительное приращение
-				else
-				{           //отрицательное приращение
-					delta += a * (2 * x + 1) - 1;
-					y++;
-				}
-				x++;
-			}
-			while (y < 60)          //пока вектор касательной принадлежит
-			{               //четвертой октанте
-				point(x, y);        //ставим точку с координатами (x,y)
-				if (delta >= 0)
-					delta += -1;    //отрицательное приращение
-				else
-				{           //положительное приращение
-					delta += a * (2 * x + 1) - 1;
-					x++;
-				}
-				y++;
-			}
 
-		}
-
-		// Генерация точек прямой методом приращений
-		/*
+		//Генерация точек прямой методом приращений
 		void line8(int x0, int y0, int x1, int y1)
 		{
 			int x = x0, y = y0;         // 8-связный отрезок
 			int dx = x1 - x0, dy = y1 - y0;
+
 			int delta = 0;          // значение функции
-			int ex = sign(dx), ey = sign(dy);   // знаки приращений
+
+			int ex = sign(dx); 
+			int ey = sign(dy);   // знаки приращений
 
 			point(x, y);
 
 			// Если dx = 0, то выводится вертикальный отрезок
-			if (dx == 0) {
-				while (y != y1) {
-					point(x, ++y);
-				}
-				return; 
-			}
+			if (dx == 0) { while (y != y1) point(x, ++y); return; }
 
 			// Если dy = 0, то выводится горизонтальный отрезок
-			if (dy == 0) {
-				while (x != x1) { 
-					point(++x, y); 
-				}
-				return; 
-			}
+			if (dy == 0) { while (x != x1) point(++x, y); return; }
 
 			// Если abs(dx) = abs(dy):
-			if (Math.Abs(dx) == Math.Abs(dy)) {
-				while (x != x1) {
-					x += ex; y += ey; 
+			if (Math.Abs(dx) == Math.Abs(dy))
+			{
+				while (x != x1) { 
+					x += ex; 
+					y += ey;
 					point(x, y); 
 				}
 				return;
 			}
-
 			// Основной цикл
 			while (!(x == x1 && y == y1))
 			{
-				// Если значение функции <=0
-				if (delta <= 0)
+				if (delta <= 0) // если значение функции <=0
 				{
 					if (dy * ex - dx * ey > 0) // положит. перемещение
 					{
-						x += ex; y += ey; delta += dy * ex - dx * ey;
+						x += ex; y += ey; 
+						delta += dy * ex - dx * ey;
 						point(x, y);    // перемещение по x и y
 					}
-					else        // иначе перемещение по x
-					  if (Math.Abs(dx) > Math.Abs(dy))
+					// иначе перемещение по x
+					else
 					{
-						x += ex; delta += dy * ex;
-						point(x, y);
-					}
-					else    // или по y
-					{
-						y += ey; delta -= dx * ey;
-						point(x, y);
+						if (Math.Abs(dx) > Math.Abs(dy))
+						{
+							x += ex; 
+							delta += dy * ex;
+							point(x, y);
+						}
+						else    // или по y
+						{
+							y += ey; 
+							delta -= dx * ey;
+							point(x, y);
+						}
 					}
 				}
-
-				// Если значение функции > 0
-				else
-				{
+				// если значение функции > 0
+				else {
 					if (dy * ex - dx * ey < 0)
 					{   // делаем два перемещения
-						x += ex; y += ey; delta += dy * ex - dx * ey;
+						x += ex;
+						y += ey;
+						delta += dy * ex - dx * ey;
 						point(x, y);
 					}
 					else
+					{
 						if (Math.Abs(dx) > Math.Abs(dy))
-					{
-						x += ex; delta += dy * ex;
-						point(x, y); // или по x
-					}
-					else
-					{
-						y += ey; delta -= dx * ey;
-						point(x, y); // или по y
+						{
+							x += ex; delta += dy * ex;
+							point(x, y); // или по x
+						}
+						else
+						{
+							y += ey; delta -= dx * ey;
+							point(x, y); // или по y
+						}
 					}
 				}
 			}
 		}
-		*/
 
-		// Функция вывода отрезка по методу
-		// приращений, использующему четыре перемещения
+		// Функция вывода отрезка по методу приращений, использующему четыре перемещения
 		void line4(int x0, int y0, int x1, int y1)
 		{
 			int dx = Math.Abs(x1 - x0), dy = Math.Abs(y1 - y0);
 			int ex, ey;
 
-			if (x1 > x0) ex = 1;
-			else ex = -1;
-
-			if (y1 > y0) ey = 1;
-			else ey = -1;
+			if (x1 > x0) ex = 1; else ex = -1;
+			if (y1 > y0) ey = 1; else ey = -1;
 
 			int E = 0, x = x0, y = y0;
 
@@ -212,70 +203,57 @@ namespace lab3
 				point(x, y);    // перемещение, изменяющее знак
 
 				if (E > 0) { 
-					E -= dx; 
-					y += ey; 
+					E -= dx; y += ey; 
 				} 
 				else { 
-					E += dy;
-					x += ex; 
+					E += dy; x += ex;
 				}
 			}
 			point(x, y);
 		}
 
-		void check()
+		// Простой алгоритм затравки с использованием рекурсии
+		void fillWithSeed(Color oldColor, Color newColor, int x, int y) {
+			
+			if (myBitmap.GetPixel(x, y) == oldColor || myBitmap.GetPixel(x, y) == newColor) return;
+			
+			myBitmap.SetPixel(x, y, newColor);
+			fillWithSeed(oldColor, newColor, x - 1, y);
+			fillWithSeed(oldColor, newColor, x + 1, y);
+			fillWithSeed(oldColor, newColor, x, y - 1);
+			fillWithSeed(oldColor, newColor, x, y + 1);	
+		}
+
+		// Построчный алгоритм затравки с использованием рекурсии
+		void fillLineWithSeed(Color oldColor, Color newColor, int x, int y)
 		{
+			if (myBitmap.GetPixel(x, y) == oldColor || myBitmap.GetPixel(x, y) == newColor) return;
 
-			int a = 48; int b = 32;
-			int a2 = a * a, b2 = b * b,
-
-			x = a, y = 0, delta = 0;  // начальная точка (x0,y0))
-
-			while (b2 * x > a2 * y) // пока вектор касательной принадлежит // третьей октанте
-			{               
-				point(x, y);        // ставим точку (x,y)
-				if (delta < 0)
-					delta += a2 * (y * 2 + 1); // положительное приращение
-				else
-				{   // отрицательное приращение
-					delta += a2 * (y * 2 + 1) + b2 * (-x * 2 + 1);
-					x--;    // x уменьшается на 1
-				}
-				y++;        // y увеличивается на 1
-			}
-			// пока вектор касательной принадлежит четвертрй октанте
-			while (x >= 0)      
+			int minX = x, maxX = x;
+			
+			while (myBitmap.GetPixel(minX, y) != oldColor) 
 			{
-				// ставим точку (x,y)
-				point(x, y);
-
-				// отрицательное приращение
-				if (delta >= 0) delta += b2 * (-x * 2 + 1);
-				else
-				{          
-					// положительное приращение
-					delta += a2 * (y * 2 + 1) + b2 * (-x * 2 + 1);
-
-					// y увеличивается на 1
-					y++;
-				}
-				// x уменьшается на 1
-				x--;
+				minX--;
 			}
+
+			while (myBitmap.GetPixel(maxX, y) != oldColor)
+			{
+				maxX++;
+			}
+
+			Pen redPen = new Pen(Color.Red);
+			graph.DrawLine(redPen, minX, y, maxX, y);
+
+
+			int index = 1;
+			fillLineWithSeed(oldColor, newColor, minX + index, y + 1);
+			fillLineWithSeed(oldColor, newColor, maxX - index, y + 1);
+			fillLineWithSeed(oldColor, newColor, (maxX + minX)/2, y + 1);
+
+			fillLineWithSeed(oldColor, newColor, minX + index, y - 1);
+			fillLineWithSeed(oldColor, newColor, maxX - index, y - 1);
+			fillLineWithSeed(oldColor, newColor, (maxX + minX) / 2, y - 1);
 		}
-
-		private void Form1_Load(object sender, EventArgs e)
-		{
-			//line8(10, 10, 50, 50);
-
-			//line8(400, 400, 200, 200);
-
-			//line8(100, 100, 300, 300);
-
-			lineg();
-		}
-
-
 
 	}
 }
